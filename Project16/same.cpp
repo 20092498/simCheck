16ac715f0418005120bdb72ce3e819e265e0c0df
hun <intelhsh@naver.com>
2024-05-31 10:38:48 +0900

    save

---
+++ Project16/same.cpp
@@ -0,0 +1,32 @@
+#include<string>
+
+using std::string;
+
+class simChecker {
+public:
+	int lengthCheck(string user) {
+		int ansLen = Ans.length();
+		int userLen = user.length();
+
+		int shortLen = ansLen > userLen ? userLen : ansLen;
+
+		int diff = std::abs(ansLen - userLen);
+
+		if (diff == 0)
+			return 60;
+
+		return (1-(diff/(float)shortLen))*60;
+	}
+
+
+	void setAns(string ans) {
+		Ans = ans;
+	}
+	string getAns()
+	{
+		return Ans;
+	}
+
+private:
+	string Ans;
+};
---
+++ Sample-Test1/simchecker_test.cpp
@@ -0,0 +1,45 @@
+#include "pch.h"
+#include "../Project16/same.cpp"
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::string;
+
+class simCheckerFixture :public testing::Test {
+public:
+	simChecker sim;
+	string ans = "BBCCDD";
+};
+
+
+TEST_F(simCheckerFixture, setAns) {
+	sim.setAns(ans);
+	string exp = sim.getAns();
+
+	EXPECT_EQ(ans, exp);
+}
+
+TEST_F(simCheckerFixture, smaeLenTest) {
+	string user = "ABCDEF";
+	sim.setAns(ans);
+	int ret = sim.lengthCheck(user);
+
+	EXPECT_EQ(ret, 60);
+}
+
+TEST_F(simCheckerFixture, UserShortdiffLenTest) {
+	string user = "ABCD";
+	sim.setAns(ans);
+	int ret = sim.lengthCheck(user);
+
+	EXPECT_EQ(ret, 30);
+}
+
+TEST_F(simCheckerFixture, AnsShortdiffLenTest) {
+	string user = "ABCDEFGH";
+	sim.setAns(ans);
+	int ret = sim.lengthCheck(user);
+
+	EXPECT_EQ(ret, 39);
+}
